4b0e3ca38c183e032e8398264839283763fdef8a
Raeef Chowdhury <raeef.chowdhury1@education.nsw.gov.au>
2025-02-08 17:44:57 +1100

    add card list UI

--- src/script.js
+++ src/script.js
@@ -1,5 +1,21 @@
 const input = document.querySelector(".search__input");
 const searchbar = document.querySelector(".searchbar");
+const bookMarkIconBox = document.querySelectorAll(".card__item--icon");
+const bookMarkIcon = document.querySelectorAll(".bookmark__icon");
+console.log(bookMarkIcon, bookMarkIconBox);
+
+bookMarkIconBox.forEach((icon, index) =>
+  icon.addEventListener("click", function () {
+    addBookMarkStyle(bookMarkIcon[index]);
+  })
+);
+function addBookMarkStyle(icon) {
+  if (icon.getAttribute("name") === "star-outline") {
+    icon.setAttribute("name", "star");
+  } else {
+    icon.setAttribute("name", "star-outline");
+  }
+}
 
 searchbar.addEventListener("click", function () {
   input.classList.toggle("w-[25rem]");
