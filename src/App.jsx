1814808899e43632c6ecaa55d237d58969e4e79a
Raeef Chowdhury <raeef.chowdhury1@education.nsw.gov.au>
2025-09-30 14:27:46 +1000

    Init API, fix small UI bugs

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,47 @@ import "./Components/Header";
 import "./Components/Search";
 import Header from "./Components/Header";
 import Search from "./Components/Search";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 /* eslint-disable react/prop-types */
+// API ENDPOINT = https://api.themoviedb.org/3/search/movie
 
 function App() {
   const [searchTerm, setSearchTerm] = useState("");
+  const [movieList, setMovieList] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const API_URL = "https://api.themoviedb.org/3";
+  const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
+  const API_OPTIONS = {
+    method: "GET",
+    headers: {
+      accept: "application/json",
+      Authorization: `Bearer ${API_KEY}`,
+    },
+  };
+  const fetchMovies = async () => {
+    try {
+      const endpoint = `${API_URL}/discover/movie?sort_by=populairty,desc`;
+      const response = await fetch(endpoint, API_OPTIONS);
+      if (!response.ok) {
+        throw new Error("cant fetch movies");
+      }
+      const data = await response.json();
+      console.log(data);
+      if (data.Response === "False") {
+        setMovieList([]);
+        return;
+      }
+      setMovieList(data.results);
+    } catch (err) {
+      console.error(`${err}`);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+  useEffect(() => {
+    fetchMovies();
+  }, []);
+
   return (
     <>
       <main className="flex justify-center items-start ">
@@ -15,6 +51,19 @@ function App() {
         <div className="wrapper flex justify-center mt-[4rem]">
           <Header />
           <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+          <section className="movies__list">
+            {isLoading ? (
+              <p className="text-white">Loading..</p>
+            ) : (
+              <ul>
+                {movieList.map((movie) => (
+                  <li key={movie.id} className="text-white">
+                    {movie.title}
+                  </li>
+                ))}
+              </ul>
+            )}
+          </section>
         </div>
       </main>
     </>
--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -2,12 +2,13 @@ function Header() {
   return (
     <>
       <header className="mt-[4rem]">
-        <div className="img__box flex flex-col gap-[1.2rem] items-center justify-center">
+        <div className="img__box flex flex-col gap-[3.6rem] items-center justify-center ">
           <img src="logo.png" alt="logo img" className="w-[10%] " />
-          <img src="hero-img.png" alt="hero__img" />
+          <img src="hero-img.png" alt="hero__img" className="scale-125" />
         </div>
-        <h1 className="">
-          Find <span className="text-gradient">Movies</span>
+        <h1 className="text-[5.6rem] leading-snug tracking-wide  ">
+          Find
+          <span className="text-gradient pr-[1.2rem] movie-span"> Movies</span>
           You will Enjoy Without the Hassle
         </h1>
       </header>
